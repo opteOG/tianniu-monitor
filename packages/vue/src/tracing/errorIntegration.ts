172db9c61c71765c208d790f593468fa5aab5ff1
niutianle <niutianle@9377.com>
2026-02-04 17:12:20 +0800

    feat: vue/react环境工具包

---
+++ packages/vue/src/tracing/errorIntegration.ts
@@ -0,0 +1,56 @@
+import { Transport } from '@tianniu-monitor/monitor-core'
+
+export interface UnhandledRejectionErrorPayload {
+  event_type: string
+  stack: string
+  message: string | Event
+  path: string
+}
+
+export interface VueErrorPayload {
+  event_type: string
+  err: unknown
+  vm: any
+  info: string
+}
+
+export class Errors {
+  constructor(private transport: Transport) {}
+
+  init(vueInstance?: any) {
+    // 监听全局错误
+    window.onerror = (message, source, lineno, colno, error) => {
+      const payload: UnhandledRejectionErrorPayload = {
+        event_type: 'error',
+        stack: error?.stack || '',
+        message,
+        path: window.location.pathname,
+      }
+      this.transport.send({ ...payload })
+    }
+
+    // 监听未处理的 Promise 拒绝事件
+    window.onunhandledrejection = event => {
+      const payload: UnhandledRejectionErrorPayload = {
+        event_type: 'error',
+        stack: event?.reason?.stack || '',
+        message: event?.reason?.message || '',
+        path: window.location.pathname,
+      }
+      this.transport.send({ ...payload })
+    }
+
+    // 监听 Vue内部 错误
+    if (vueInstance) {
+      vueInstance.config.errorHandler = (err: unknown, vm: any, info: string) => {
+        const payload: VueErrorPayload = {
+          event_type: 'vue_error',
+          err,
+          vm,
+          info,
+        }
+        this.transport.send({ ...payload })
+      }
+    }
+  }
+}
